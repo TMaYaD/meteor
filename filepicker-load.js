de57d26036ae9192788651a021f26aefcab3779c
TMaYaD <TMaYaD+github@gmail.com>
2013-04-03 15:46:22 +0530

    Load on demand

--- filepicker-load.js
+++ filepicker-load.js
@@ -1,23 +1,27 @@
-//Functions to run after the script tag has loaded
-var filepickerLoadCallback = function(){
-    //filepicker.setKey("YOUR_API_KEY_HERE");
+// Call Filepicker to load
+// pass callback functions to run after the script tag has loaded
+//
+// Filepicker(functionfunction(){
+//   filepicker.setKey("YOUR_API_KEY_HERE");
+// }, function(error){
+//   if(typeof console != undefined) {
+//     console.log(error);
+//   }
+// });
+
+var FilePicker;
+
+FilePicker = function(successCallback, failureCallback) {
+  var head, script;
+  
+  //Generate a script tag
+  script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = '//api.filepicker.io/v1/filepicker.js';
+  script.onload = successCallback;
+  script.onerror = failureCallback;
+
+  //Load the script tag
+  head = document.getElementsByTagName('head')[0];
+  return head.appendChild(script);
 };
-
-//If the script doesn't load
-var filepickerErrorCallback = function(error){
-    if(typeof console != undefined) {
-        console.log(error);
-    }
-};
-
-//Generate a script tag
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = '//api.filepicker.io/v1/filepicker.js';
-script.onload = filepickerLoadCallback;
-script.onerror = filepickerErrorCallback;
-
-//Load the script tag
-var head = document.getElementsByTagName('head')[0];
-head.appendChild(script);
-
